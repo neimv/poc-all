3b1bf60f92cd5818be54e833785f1a658986ec5e
neimv <neimv.zatara@gmail.com>
2023-07-05 18:34:43 -0600

    creating users

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/neimv/poc-all/views"
 )
 
 var PORT = ":8000"
@@ -14,6 +15,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!!")
 	})
+	e.GET("/users", views.GetUsers)
+	e.GET("/users/:id", views.GetUsersId)
 
 	e.Logger.Fatal(e.Start(PORT))
 }
